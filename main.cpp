0a8bd7f6aa4b140ca89f9fa575582c6b2dd474c0
Chernyackoff <chernyackoff.vlad@yandex.ru>
2022-04-09 13:49:33 +0300

    Lab3 instead Lab2

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <QPoint>
 #include <QKeyEvent>
 #include <cmath>
+#include <vector>
 
 const int delta_move = 10;
 const double rotation_angle = 15 * std::numbers::pi / 180;
@@ -21,9 +22,8 @@ const std::array<QPointF, 6> default_points = {QPointF(10, 10),
 
 class Hexagon {
 public:
-
     Hexagon(QGraphicsScene *scene_, const std::array<QPointF, 6> &points_) : scene(scene_), points(points_), lines(),
-                                                                             centre() {
+                                                                             centre(), paintLines(){
         double cx = 0, cy = 0;
 //        QString letter = "ABCDEF";
         for (int i = 0; i < 6; i++) {
@@ -37,6 +37,7 @@ public:
         }
         centre.setX(cx / 6);
         centre.setY(cx / 6);
+        paint();
     }
 
     ~Hexagon() {
@@ -84,14 +85,77 @@ private:
     QGraphicsScene *scene;
     std::array<QPointF, 6> points;
     std::array<QGraphicsLineItem *, 6> lines;
+    std::vector<QGraphicsLineItem *> paintLines;
     QPointF centre;
 //    std::array<QGraphicsTextItem *,6> letters;
 
+    void paint(){
+        QPen pen;
+        pen.setColor(Qt::green);
+        pen.setStyle(Qt::SolidLine);
+        // 1rst triangulation pattern
+//        draw(points[0], points[1], points[4]);
+//        draw(points[1], points[2], points[3]);
+//        draw(points[1], points[3], points[4]);
+//        draw(points[4], points[5], points[0]);
+        // 2nd triangulation pattern
+        for (int i = 1; i < lines.size() - 1; i++){
+            draw(points[0], points[i], points[i+1], pen);
+        }
+    }
+    void draw(QPointF p1, QPointF p2, QPointF p3, QPen pen){
+        // sort in ascending order from p1 to p3
+        if (p1.y() > p2.y()) std::swap(p1, p2);
+        if (p2.y() > p3.y()) std::swap(p2, p3);
+        if (p2.y() < p1.y()) std::swap(p2, p1);
+
+        // ratios of line equation i.e (x - x1)/(x2 - x1) = (y - y1)/(y2 - y1)
+        // kX = x2 - x1
+        // ky = y2 - y1
+        int kX1, kX2, kY1, kY2;
+        int x0 = p1.x(), y0 = p1.y();
+
+        bool switched = false;
+
+        kX1 = p2.x() - p1.x();
+        kY1 = p2.y() - p1.y();
+        kX2 = p3.x() - p1.x();
+        kY2 = p3.y() - p1.y();
+
+        // intersection points
+        qreal x1, x2;
+        for (int y = p1.y() + 1; y <= p3.y(); y += 1) {
+            if (y >= int(p2.y()) || kY1 == 0) {
+                switched = true;
+                kX1 = p3.x() - p2.x();
+                kY1 = p3.y() - p2.y();
+                x0 = p2.x();
+                y0 = p2.y();
+            }
+
+            x1 = double ((y - y0) * kX1) / kY1 + x0;
+            x2 = double ((y - p1.y()) * kX2) / kY2 + p1.x();
+
+            if (kX1 == 0) x1 = switched ? p2.x() : p1.x();
+            if (kX2 == 0) x2 = p1.x();
+
+            paintLines.push_back(scene->addLine(
+                    {QPointF(x1, y), QPointF(x2, y)}, pen));
+        }
+    }
+
     void update() {
+        for(auto line : paintLines){
+            scene->removeItem(line);
+            delete line;
+        }
+        paintLines.clear();
         for (int i = 0; i < 6; i++) {
             lines[i]->setLine({points[i], points[(i + 1) % 6]});
 //            letters[i]->setPos(points[i]);
+
         }
+        paint();
     }
 };
 
